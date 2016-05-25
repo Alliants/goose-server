1b4ec469708a6f3c6b37ec7f05c2dcc1db040e23
Alvaro F. Lara <alvaro.lara@alliants.com>
2016-05-25 20:47:25 +0100

    Fix seed problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-require "refresh_data"
-
 RepositoryStorage.destroy_all
-dr = DataRefresher.new(Repository.all(cache: false))
+dr = DataRefresher.new(repositories: Repository.all(cache: false))
 dr.refresh_all!
