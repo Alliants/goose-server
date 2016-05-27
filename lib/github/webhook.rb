f304818cc0beec8a4d41ce09b367fbe660b43ee5
Alvaro F. Lara <alvaro.lara@alliants.com>
2016-05-27 17:18:59 +0100

    Move helper class into another file

--- lib/github/webhook.rb
+++ lib/github/webhook.rb
@@ -1,20 +1,7 @@
+require "github/webhook/pull_request_handler"
+
 module Github
   class Webhook
-    class PullRequestHandler
-      def self.parse(payload)
-        PullRequestStorage.create(
-          link: payload[:pull_request][:html_link],
-          title: payload[:pull_request][:title],
-          org: payload[:pull_request][:head][:user][:login],
-          repo: payload[:pull_request][:head][:repo][:full_name],
-          owner: payload[:pull_request][:user][:login],
-          created_at: payload[:pull_request][:created_at],
-          original_id: payload[:pull_request][:id],
-          number_of_comments: payload[:pull_request][:review_comments]
-        )
-      end
-    end
-
     TYPE_MAP = {
       "pull_request" => PullRequestHandler
     }.freeze
@@ -22,7 +9,7 @@ module Github
     def self.call(payload:, type:)
       handler = TYPE_MAP.fetch(type)
 
-      handler.parse(payload)
+      handler.new(payload).save
     end
   end
 end
---
+++ lib/github/webhook/pull_request_handler.rb
@@ -0,0 +1,58 @@
+module Github
+  class Webhook
+    class PullRequestHandler
+      def initialize(payload)
+        @payload = payload
+      end
+
+      def link
+        payload[:pull_request][:html_link]
+      end
+
+      def title
+        payload[:pull_request][:title]
+      end
+
+      def organization
+        payload[:pull_request][:head][:user][:login]
+      end
+
+      def repository
+        payload[:pull_request][:head][:repo][:full_name]
+      end
+
+      def owner
+        payload[:pull_request][:user][:login]
+      end
+
+      def created_at
+        payload[:pull_request][:created_at]
+      end
+
+      def original_id
+        payload[:pull_request][:id]
+      end
+
+      def number_of_comments
+        payload[:pull_request][:review_comments]
+      end
+
+      def save
+        PullRequestStorage.create(
+          link: link,
+          title: title,
+          org: organization,
+          repo: repository,
+          owner: owner,
+          created_at: created_at,
+          original_id: original_id,
+          number_of_comments: number_of_comments
+        )
+      end
+
+      private
+
+      attr_reader :payload
+    end
+  end
+end
